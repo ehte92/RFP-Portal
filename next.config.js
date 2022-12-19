51343c161532575dc0eb8a89efe51598effbcf23
Ehtesham Ansari <46630915+ehte92@users.noreply.github.com>
2022-12-19 14:14:06 +0000

    updated next.config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   reactStrictMode: true,
   env: {
-    NEXT_PUBLIC_API_URL: "https://rfp-portal-api.vercel.app/sendemail",
+    NEXT_PUBLIC_API_URL: "https://rfp-portal-api.onrender.com/sendemail",
   },
 };
